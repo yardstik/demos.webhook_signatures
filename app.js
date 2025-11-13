9c8c3fc7f610907581f6634bb69f4d5934880912
Sky Lukas <sky.lukas@yardstik.com>
2025-11-13 10:57:11 -0600

    fix: clarify http status codes

--- app.js
+++ app.js
@@ -11,26 +11,27 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+    console.log('Webhook received, verifying signature...');
     const SIGNATURE = req.get('x-yardstik-webhook-signature');
     if (!SIGNATURE) {
         console.log('Missing x-yardstik-webhook-signature header');
-        res.sendStatus(401);
+        res.sendStatus(200); // Log and ignore, but respond with 200 to avoid retries
         return;
     }
 
     const BODY = req.body;
     const HASH = crypto.createHmac('sha256', process.env.WEBHOOK_SIGNATURE).update(JSON.stringify(BODY)).digest('hex');
 
-    if (SIGNATURE !== HASH) {
-        console.log('Invalid signature');
-        res.sendStatus(401);
-        return;
-    }
-
-    // Your code here
     console.log(JSON.stringify(BODY, null, 2))
     console.log(`This is the x-yardstik-webhook-signature: ` + SIGNATURE);
     console.log(`This is your application's calculated hash: ` + HASH);
+
+    if (SIGNATURE !== HASH) {
+      console.log('Invalid signature');
+    } else {
+      // Your code here...
+      console.log('Valid signature');
+    }
 
     // Respond with a 200, if you do not respond with a success code Yardstik will retry the request.
     res.sendStatus(200);
